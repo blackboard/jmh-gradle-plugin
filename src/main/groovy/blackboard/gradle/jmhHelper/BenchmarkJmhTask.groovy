7150906ac44ab5daf9d5fcefcfebe4d25883d0b2
Sam Bishop <Sam.Bishop@blackboard.com>
2014-04-24 16:52:21 -0400

    Cleaned up BenchmarkJmhTask.groovy. Had a lot of un-needed imports.

--- src/main/groovy/blackboard/gradle/jmhHelper/BenchmarkJmhTask.groovy
+++ src/main/groovy/blackboard/gradle/jmhHelper/BenchmarkJmhTask.groovy
@@ -2,18 +2,12 @@ package blackboard.gradle.jmhHelper
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
-import org.gradle.process.ExecResult;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.JavaExec;
-import org.gradle.api.plugins.JavaPlugin;
-//import org.openjdk.jmh.runner.Runner;
-//import org.openjdk.jmh.runner.options.Options;
-//import org.openjdk.jmh.runner.options.OptionsBuilder;
-//import org.openjdk.jmh.Main;
 
-//import org.apache.commons.exec.*;
 import java.io.File;
 
+/** This task is used to run JMH benchmark files.  */
 
 class BenchmarkJmhTask extends DefaultTask /*JavaExec */{
   /* Task which identifies all benchmark class files, and 
@@ -21,8 +15,8 @@ class BenchmarkJmhTask extends DefaultTask /*JavaExec */{
   
   private static final JMH_RUNNER = "org.openjdk.jmh.Main"
 
-  String outputFile = "$project.buildDir/jmh-output.txt";
-  def jexec = new JavaExec();
+  private String outputFile = "$project.buildDir/jmh-output.txt";
+  private def jexec = new JavaExec();
  
   @TaskAction
   def benchmarkJmh() {
@@ -37,15 +31,6 @@ class BenchmarkJmhTask extends DefaultTask /*JavaExec */{
     jexec.classpath = project.sourceSets.benchmark.output + project.configurations.benchmarkRuntime
     new File(outputFile).getParentFile().mkdirs()
     jexec.args(['-o', outputFile])
-    //jexec.setIgnoreExitValue(true);
-    println "In the task, I really hope jexec can find the main class now.";
     jexec.exec();
-    //classPath = project.sourceSets.benchmark.output + project.configurations.benchmarkRuntime
-    //Find a way for the plugin to include the actual org.openjdk.jmh.Main in the plugin jar file.
-    //Actually exec the task.
-    //this.setMain("org.openjdk.jmh.Main"); 
-    //args = ['-h'];
-    // Options opt = new OptionsBuilder().include(".*").forks(1).build();
-    //new Runner(opt).run();
   }
 }
