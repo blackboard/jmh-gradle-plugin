f462819e0715bdf0855e4e1dd015282897581a34
Sam Bishop <Sam.Bishop@blackboard.com>
2014-05-07 10:02:21 -0400

    Refactored BenchmarkJmhTask into a JavaExec task instead of a DefaultTask. This should hopefully allow jvmArgs to be set in build.gradle files.

--- src/test/groovy/blackboard/gradle/jmhHelper/BenchmarkJmhTaskTest.groovy
+++ src/test/groovy/blackboard/gradle/jmhHelper/BenchmarkJmhTaskTest.groovy
@@ -1,15 +1,14 @@
 package blackboard.gradle.jmhHelper
 
 import com.blackboard.gradle.BenchmarkJmhTask
-import org.gradle.api.Project;
+import org.gradle.api.Project
+import org.gradle.api.tasks.JavaExec;
 import org.gradle.testfixtures.ProjectBuilder
-import org.gradle.api.DefaultTask;
 
 import org.junit.Test;
 import org.junit.Before
 
 import static org.junit.Assert.*;
-
 
 class BenchmarkJmhTaskTest {
 
@@ -29,14 +28,24 @@ class BenchmarkJmhTaskTest {
 
   @Test
   public void testTaskIsTypeJavaExec() {
-    assertTrue(task instanceof DefaultTask); 
+    assertTrue(task instanceof JavaExec);
   }
 
   @Test
-  public void testHelpArg() {
-    project.apply plugin: 'jmh'
-    project.setProperty("help", "");
-    task.benchmarkJmh();
+  public void testApplyJVMArgs(){
+    project.apply plugin: com.blackboard.gradle.JMHPlugin
+    project.setProperty("-jvmArgs", "-Xmx2048m -Xms1024m");
+    BenchmarkJmhTask t = task;
+    t.benchmarkJmh();
+    assertTrue(t.getMinHeapSize().equals("1024m"));
+    assertTrue(t.getMaxHeapSize().equals("2048m"));
   }
 
+  @Test
+  public void testPrintDefaultJvmArgs(){
+    project.apply plugin: com.blackboard.gradle.JMHPlugin
+    BenchmarkJmhTask t = task;
+    println "Banana"
+    t.getAllJvmArgs().each { println it}
+  }
 } 
